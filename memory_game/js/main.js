c6677adef1f076fbc0a8547d56754b3385fbc9e5
garricktgh <garrick@hotmail.sg>
2019-08-27 22:39:08 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,29 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
+// var cardOne = cards[0];
+// var cardTwo = cards[2];
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again.");
+		}
 }
+// cardsInPlay.push(cardOne);
+// cardsInPlay.push(cardTwo);
+
+function flipCard(cardID) {
+	// console.log("User flipped " + cardsInPlay[0]);
+	// console.log("User flipped " + cardsInPlay[1]);
+	cardsInPlay.push(cardID);
+
+	console.log("User flipped " + cardID);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}	
+}
+
+flipCard(cards[0]);
+flipCard(cards[2]);
+
